32e908ed3bbc407a1b037afaf08ab2fb4e56305b
ToQuery <toquery@qq.com>
2022-06-22 17:18:05 +0800

    初始化项目

---
+++ src/main/java/io/github/toquery/example/spring/reactive/ExampleSpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/reactive/ExampleSpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
